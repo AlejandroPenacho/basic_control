2a0999d1ebc943415edff4753ca8f8b34bd49863
Alejandro <alejpr@kth.se>
2023-08-27 21:58:26 +0200

    Zoom and scroll for plots

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
     let nyquist_values;
 
     let side_gone = 0;
+    let zoom_level = 1;
     let diff_equation;
 
     $: {
@@ -30,16 +31,23 @@
     }
 
     let canvas;
+    let canvas_conf = {
+        x_0: 0,
+        y_0: 0,
+        x_scale: 2,
+        y_scale: 2
+    };
+    
     let second_canvas;
 
-    let t_array = Array.from({length: 10000}, (e, i) => i/1000)
-    let y_array = Array.from(10000)
+    let t_array = Array.from({length: 100000}, (e, i) => i/10000)
+    let y_array = Array.from(100000)
 
     function update_trajectory() {
-        let dt = 0.01;
+        let dt = 0.00001;
         y_array[0] = 0;
         let state = [0, 0];
-        for (let i = 1; i < 10000; i++) {
+        for (let i = 1; i < 100000; i++) {
             state[0] = state[0] + state[1] * dt;
             state[1] = state[1] + my_num * 2 * (1 - state[1]) - 4 * state[0];
             y_array[i] = state[0];
@@ -59,11 +67,24 @@
 
     function draw_nyquist() {
         let ctx = canvas.getContext("2d");
+        let x_scale = canvas_conf.x_scale * canvas.width/2;
+        let y_scale = canvas_conf.y_scale * canvas.height/2;
+
+        let x_0 = canvas.width/2 + canvas_conf.x_scale * canvas_conf.x_0;
+        let y_0 = canvas.height/2 + canvas_conf.y_scale * canvas_conf.y_0;
+
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath()
-        ctx.moveTo(100*math.re(nyquist_values[0]) + 250, 100*math.im(nyquist_values[0]) + 250);
+        ctx.moveTo(
+            x_scale*math.re(nyquist_values[0]) + x_0,
+            -y_scale*math.im(nyquist_values[0]) + y_0,
+        );
+
         for (let i=1; i < nyquist_values.length; i++) {
-            ctx.lineTo(100*math.re(nyquist_values[i]) + 250, -100*math.im(nyquist_values[i]) + 250);
+            ctx.lineTo(
+                x_scale*math.re(nyquist_values[i]) + x_0,
+                -y_scale*math.im(nyquist_values[i]) + y_0,
+            );
         }
         ctx.stroke();
     }
@@ -77,7 +98,45 @@
         update_trajectory();
     }
 
-    
+    function canvas_scroll(e) {
+        if (e.deltaY > 0) {
+            canvas_conf.x_scale *= 0.8;
+            canvas_conf.y_scale *= 0.8;
+        } else {
+            canvas_conf.x_scale /= 0.8;
+            canvas_conf.y_scale /= 0.8;
+        }
+        draw_nyquist();
+    }
+
+    let ref_mouse_point = [0, 0];
+    let mouse_point_pressed = false;
+    function canvas_mouse_down(e) {
+        mouse_point_pressed = true;
+        ref_mouse_point = [
+            e.clientX,
+            e.clientY
+        ]
+    }
+    function canvas_mouse_up(e) {
+        mouse_point_pressed = false;
+    }
+    function canvas_mouse_move(e) {
+        if (!mouse_point_pressed) {
+            return
+        }
+        let delta_X = e.clientX - ref_mouse_point[0];
+        let delta_Y = e.clientY - ref_mouse_point[1];
+        ref_mouse_point = [
+            e.clientX,
+            e.clientY
+        ]
+        canvas_conf.x_0 += delta_X / canvas_conf.x_scale;
+        canvas_conf.y_0 += delta_Y / canvas_conf.y_scale;
+        console.log(canvas_conf.x_0)
+        draw_nyquist();
+    }
+
 </script>
 
 <style>
@@ -90,6 +149,8 @@
 </style>
 
 This is a {x}
+<br>
+Hej check {nyquist_values[0]}
 
 Hail the pole:
 <br>
@@ -116,7 +177,15 @@ Hail the pole:
     min="-10"
     max="10"
 />
-
+<br>
+<input
+    type="range"
+    bind:value={zoom_level}
+    on:input={draw_nyquist}
+    step="0.05"
+    min="0.2"
+    max="4"
+/>
 <br>
 And the {nyquist_values[4]}
 <br>
@@ -125,9 +194,12 @@ And the {nyquist_values[4]}
     <div id="myCanvas">
     <canvas
         bind:this={canvas}
+        on:wheel={canvas_scroll}
+        on:mousedown={canvas_mouse_down}
+        on:mouseup={canvas_mouse_up}
+        on:mousemove={canvas_mouse_move}
         width={500}
         height={500}
-        style="transform: translate({side_gone}cm)"
     />
     </div>
 
